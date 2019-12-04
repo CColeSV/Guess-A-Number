3fec206fc4760163e72ee3e48acb2cc33b71559d
Craig Coleman <CColeSV07@Gmail.com>
2019-12-03 23:43:27 -0500

    setting up foundations

---
+++ src/components/DirectionBox.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class DirectionBox extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>A random number has been generated.</h3>
+        <h3> Try typing in a number above..</h3>
+      </div>
+    );
+  }
+}
+
+export default DirectionBox;
---
+++ src/components/GuessApp.js
@@ -0,0 +1,28 @@
+import React from "react";
+import DirectionBox from "./DirectionBox";
+import AnimatedWindows from "./AnimatedWindows";
+
+class GuessApp extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div className="main-container">
+        <h1>App</h1>
+        <div>
+          <h2>Guess A Number!</h2>
+        </div>
+        <div className="input">
+          <input type="number"></input>
+        </div>
+        <div>
+          <DirectionBox />
+        </div>
+        <div className="windows">
+          <AnimatedWindows />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GuessApp;
