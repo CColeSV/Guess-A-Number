780994b93323dd95c4d078a9c20250efb12e2520
Craig Coleman <CColeSV07@Gmail.com>
2019-12-05 23:46:24 -0500

    CSS Update

--- src/components/DirectionBox.js
+++ src/components/DirectionBox.js
@@ -3,7 +3,7 @@ import React from "react";
 class DirectionBox extends React.Component {
   render() {
     return (
-      <div>
+      <div className="dbox">
         <h3>A random number has been generated.</h3>
         <h3> Try typing in a number above..</h3>
       </div>
